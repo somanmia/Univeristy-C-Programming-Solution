1721d664accff0a5f9c8b233b255814a53242411
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2023-09-25 08:53:55 -0700

    Update Basic_salary.c

--- Basic_salary.c
+++ Basic_salary.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+int main(){
+  float salary,basic;
+ while(scanf("%f",&basic)!=EOF){
+      if(basic<10000){
+        salary=basic+basic*50/100;
+         printf("Salary : %.2f \n",salary);
+      }else if(basic>20000 && basic<3000){
+             salary=basic+(basic*45)/100;
+              printf("Salary : %.2f \n",salary);
+        }
+        else if(basic>30000){
+            salary=basic+(basic*40)/100;
+             printf("Salary : %.2f \n",salary);
+        }else{
+          printf("Please ,Enter Valid Baisc Salary.\n");
+        }
 
+ }
+
+
+return 0;
+}
